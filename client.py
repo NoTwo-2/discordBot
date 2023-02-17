c4417fc7d8d55330063f604bfa2fa4bdfe4cb6b5
unknown <cobjaysierko@gmail.com>
2023-02-17 15:50:23 -0600

    started working on commands

--- client.py
+++ client.py
@@ -1,9 +1,32 @@
 import discord
+import constants
+
+
 
 class RPGClient(discord.Client):
+    
+
+
+    def __init__(self, prefix: str, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.prefix: str = prefix
+
+
+    # Custom Functions
+    async def __resolve_command(message: discord.Message):
+        command = message.content[1:]
+        
+        match command:
+            case _:
+                await message.channel.send("Unknown Command!")
+
     async def on_ready(self: discord.Client):
         guilds = [guild.name async for guild in self.fetch_guilds()]
         print(f"Finished logon, serving {guilds}")
     
-    # async def on_message(self: discord.client, message: discord.Message):
-    #     if message.author
+    async def on_message(self: discord.Client, message: discord.Message):
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith(constants.COMMAND_PREFIX):
+            self.__resolve_command(message)
